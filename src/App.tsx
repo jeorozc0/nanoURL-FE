311fdd6232fead713f450461b9a86185aa8f88fc
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-10-30 18:36:49 -0600

    update fetch URL in Redirect component to use hardcoded API endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
     setShortURL("");
 
     try {
-      const response = await fetch(import.meta.env.VITE_API_URL, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/url`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url: longURL }),
