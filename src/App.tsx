3b3946a388cf089bfe6e4ad931e9e5b9ceab379a
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-10-30 18:05:43 -0600

    update API URL to use environment variable and add title for copy button

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
     setShortURL("");
 
     try {
-      const response = await fetch("http://localhost:8080/url", {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url: longURL }),
@@ -102,6 +102,7 @@ export default function App() {
                 {shortURL}
               </a>
               <button
+                title="Copy to clipboard"
                 className="w-auto h-auto flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
                 onClick={handleCopyToClipboard}
               >
